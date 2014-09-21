0ec341452ce9486ee0582c6402ebdcc325800939
Bennett Kanuka <bkanuka@gmail.com>
2014-09-20 22:17:42 -0400

    works

--- comingsoon/views.py
+++ comingsoon/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext
-from comingsoon.forms import EmailForm
 
 
 def index(request):
-    form = EmailForm()
     return render_to_response('page_coming_soon.html',
-        {'form': form},
         context_instance=RequestContext(request))
 
